8a06bc18e16a5b9e544aaf5c709966fc74930ba9
Ivan Rodriguez <t-ivrodr@microsoft.com>
2017-01-17 16:26:59 +0100

    Addressing comments in CR

--- Tests/UnitTests/V2LibraryTests/FunctionTests.cpp
+++ Tests/UnitTests/V2LibraryTests/FunctionTests.cpp
@@ -8,7 +8,7 @@
 
 using namespace CNTK;
 
-namespace Microsoft { namespace MSR { namespace CNTK { namespace Test {
+namespace CNTK { namespace Test {
 
 void TestReduceSum(size_t sampleRank, const DeviceDescriptor& device)
 {
@@ -802,4 +802,4 @@ BOOST_AUTO_TEST_CASE(OutputVariableNameInCPU)
 
 BOOST_AUTO_TEST_SUITE_END()
 
-}}}}
+}}
